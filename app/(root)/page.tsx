93e5dccdbcc4dc2e989d914650ff4f57856b6fc4
TarunRaina <tarunraina.691@gmail.com>
2026-01-23 09:12:04 +0530

    Restructure app routes and update project configuration

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">SkyShelf - Organise Beyond Limits</h1>
+    </div>
+  );
+}
